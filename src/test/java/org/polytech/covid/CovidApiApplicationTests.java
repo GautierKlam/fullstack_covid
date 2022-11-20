9e55086966f73aff57d6661ad9d3ec3eea1dcb65
TENEDE Tene <tenedetene@gmail.com>
2022-11-20 14:44:18 +0100

    Correction de l'erreur de test jenkins

--- src/test/java/org/polytech/covid/CovidApiApplicationTests.java
+++ src/test/java/org/polytech/covid/CovidApiApplicationTests.java
@@ -3,7 +3,7 @@ package org.polytech.covid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class CovidApiApplicationTests {
 
 	@Test
