b22fdf94aa52d891e7c377b841a7aafefb4f8f95
TENEDE Tene <tenedetene@gmail.com>
2022-11-25 17:33:26 +0100

    ETag

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,16 +1,24 @@
-pipeline{
-    agent any
-    
-    tools {
-        jdk 'java17'
-    }
+pipeline {
+    agent none
+    stages {
 
-    stages{
-        stage('build'){
+        stage('Database') {
+            agent {
+                docker { image 'postgres:14.5' }
+            }
             steps {
-              sh './gradlew clean build'
+                sh 'node --version'
             }
         }
-       
+
+        stage('Back-end') {
+            agent {
+                docker { image 'maven:3.8.1-adoptopenjdk-11' }
+            }
+            steps {
+                sh 'mvn --version'
+            }
+        }
+        
     }
 }
